a27dbccc4299ac8976c46df27edb356ace973d68
Ethan S <58062201+shahbazianep@users.noreply.github.com>
2025-04-01 23:06:55 -0700

    Added metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,8 @@ import "./globals.css";
 
 export const metadata: Metadata = {
     title: "TFTdle",
-    description: "",
+    description:
+        "TFTdle is a daily Teamfight Tactics guessing game where you try to identify the hidden champion based on traits, cost, and other attributes. Test your knowledge and improve your strategy with every guess!",
 };
 
 export default function RootLayout({
